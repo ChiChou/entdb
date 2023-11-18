db5e08a141070cd95fdb73db889fa758d14e5fcd
cc <codecolorist@gmail.com>
2023-11-18 22:52:40 +0100

    only evaluate on MH_EXECUTE

--- entdb/magic.py
+++ entdb/magic.py
@@ -12,6 +12,20 @@ class Headers(IntEnum):
     MH_CIGAM_64 = 0xcffaedfe
 
 
+class FileType(IntEnum):
+    MH_OBJECT = 0x1
+    MH_EXECUTE = 0x2
+    MH_FVMLIB = 0x3
+    MH_CORE = 0x4
+    MH_PRELOAD = 0x5
+    MH_DYLIB = 0x6
+    MH_DYLINKER = 0x7
+    MH_BUNDLE = 0x8
+    MH_DYLIB_STUB = 0x9
+    MH_DSYM = 0xa
+    MH_KEXT_BUNDLE = 0xb
+
+
 all_values = set(val for val in Headers)
 
 
@@ -22,11 +36,11 @@ def test(value):
 def is_macho(path_or_str: Path | str):
     try:
         with Path(path_or_str).open('rb') as fp:
-            buf = fp.read(4)
-            if len(buf) < 4:
+            buf = fp.read(16)
+            if len(buf) < 16:
                 return False
     except (PermissionError, FileNotFoundError) as _:
         return False
 
-    magic, = struct.unpack('I', buf)
-    return test(magic)
+    magic, cputype, cpusubtyp, filetype = struct.unpack('IIII', buf)
+    return test(magic) and filetype == FileType.MH_EXECUTE
