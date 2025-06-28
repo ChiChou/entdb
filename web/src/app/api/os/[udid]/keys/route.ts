1234a32ba5db28ceb1128c76b2906769d5f7a70e
cc <codecolorist@gmail.com>
2025-06-28 12:58:22 +0200

    add cache

--- web/src/app/api/os/[udid]/keys/route.ts
+++ web/src/app/api/os/[udid]/keys/route.ts
@@ -6,11 +6,21 @@ export async function GET(
   request: Request,
   { params }: { params: Promise<{ udid: string }> },
 ): Promise<Response> {
-  const { DB } = getRequestContext().env;
+  const { DB, CACHE } = getRequestContext().env;
   const { udid } = await params;
   const keyword = new URL(request.url).searchParams.get("k");
 
-  const tail = keyword ? `AND p.key glob ?` : "";
+  if (!keyword) {
+    const cache = await CACHE.get(`keys/${udid}/`);
+    if (cache)
+      return new Response(cache, {
+        headers: {
+          "Content-Type": "text/plain",
+        },
+      });
+  }
+
+  const tail = keyword ? `AND p.key glob ? LIMIT 20` : "";
   const sql = `SELECT DISTINCT p.key FROM pair AS p JOIN bin AS b ON
       b.os_id = (SELECT id as os_id FROM os WHERE udid = ?) AND
       p.bin_id = b.id ${tail};`;
@@ -25,6 +35,12 @@ export async function GET(
 
   const { results } = await statement.bind(...columns).all();
   const textOnly = results.map((result) => result.key).join("\n");
+
+  if (!keyword)
+    await CACHE.put(`keys/${udid}/`, textOnly, {
+      expirationTtl: 60 * 60 * 24,
+    });
+
   return new Response(textOnly, {
     headers: {
       "Content-Type": "text/plain",
