5e7f54aaf80ca98462f80528373e02893dfd1b90
cc <codecolorist@gmail.com>
2025-08-23 02:44:02 +0200

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
 };
 
+if (typeof nextConfig.basePath === "string") {
+  process.env.NEXT_PUBLIC_BASE_PATH = nextConfig.basePath;
+}
+
 export default nextConfig;
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,14 +1,6 @@
 function getBasePath() {
-  if ("PUBLIC_BASE_PATH" in process.env) {
-    return process.env.PUBLIC_BASE_PATH;
-  }
-
-  const ownerAndRepo = process.env.GITHUB_REPOSITORY;
-  if (typeof ownerAndRepo === "string") {
-    const [owner, repo] = ownerAndRepo.split("/", 2);
-    if (repo !== `${owner}.github.io`) {
-      return `/${repo}`;
-    }
+  if ("NEXT_PUBLIC_BASE_PATH" in process.env) {
+    return process.env.NEXT_PUBLIC_BASE_PATH;
   }
 
   return "";
