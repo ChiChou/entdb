973921fa0d34130a7ad63c6d4ee30279a3290740
cc <codecolorist@gmail.com>
2025-09-01 01:57:51 +0200

    keep major and (latest patch for) minor

--- data/ipsw.me/firmwares.py
+++ data/ipsw.me/firmwares.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from pathlib import Path
+from collections import defaultdict
 import json
 
 
@@ -17,22 +18,6 @@ def fetch_with_cache(url: str, cache: str) -> bytes:
         fp.write(buf)
 
     return buf
-
-
-def keep_version(version: str) -> bool:
-    segments = version.split(".")
-    major, minor, *_ = segments
-
-    # only keep major or major.minor versions
-    if len(segments) > 2:
-        return False
-
-    # for old systems, only keep major version
-    if int(major) < 16:
-        return minor == "0"
-
-    return True
-
 
 def main():
     devices = json.loads(fetch_with_cache("https://api.ipsw.me/v4/devices", "devices"))
@@ -52,15 +37,32 @@ def main():
         # this item gets updated many times until the latest model
         for fw in ipsw["firmwares"]:
             version = fw["version"]
-            if keep_version(version):
-                unified[version] = fw["url"]
+            unified[version] = fw["url"]
 
-    matrix = list(unified.items())
-    # for ocd people
-    # matrix.sort(key=lambda x: list(map(int, x[0].split('.'))))
+    # group by major.minor
+    groups = defaultdict(list)
 
-    for verison, url in matrix:
-        print(verison, url)
+    for version, url in unified.items():
+        segments = version.split('.')
+        numbers = list(map(int, segments))
+        major, minor, *_ = segments
+        key = '%s.%s' % (major, minor)
+        groups[key].append((numbers, url))
+
+    latest_minor_versions = []
+    for key, group in groups.items():
+        group.sort(key=lambda x: x[0])
+        latest_minor_versions.append(group[-1])
+
+    # visited: set[int] = set()
+    for numbers, url in latest_minor_versions:
+        major, *_ = numbers
+        # if major < 11:
+        #     if major in visited:
+        #         continue
+        #     visited.add(major)
+
+        print('.'.join(map(str, numbers)), url)
 
 
 if __name__ == "__main__":
