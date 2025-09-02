185d955dcb2ddf22873a907fa26686ad9b604b35
cc <codecolorist@gmail.com>
2025-09-02 21:45:13 +0200

    remove requests

--- ipsw/aea.py
+++ ipsw/aea.py
@@ -3,50 +3,41 @@
 # Modified from Nicolas's initial script
 # Thx to Siguza and Snoolie for AEA auth block parsing information
 
-# Requirements: pip3 install requests pyhpke
+# Requirements: pip3 install pyhpke
 
 import argparse
 import base64
 import json
 import sys
+import urllib.request
 from pathlib import Path
 from pprint import pprint
 
-import requests
 from pyhpke import AEADId, CipherSuite, KDFId, KEMId, KEMKey
 
 AEA_PROFILE__HKDF_SHA256_AESCTR_HMAC__SYMMETRIC__NONE = 1
 
-suite = CipherSuite.new(KEMId.DHKEM_P256_HKDF_SHA256, KDFId.HKDF_SHA256, AEADId.AES256_GCM)
-
-
-def error(msg):
-    print(msg, file=sys.stderr)
-    exit(1)
+suite = CipherSuite.new(
+    KEMId.DHKEM_P256_HKDF_SHA256, KDFId.HKDF_SHA256, AEADId.AES256_GCM
+)
 
 
 def get_key(f, verbose: bool = False):
     fields = {}
     header = f.read(12)
-    if len(header) != 12:
-        error(f"Expected 12 bytes, got {len(header)}")
+    assert len(header) == 12
 
     magic = header[:4]
-    if magic != b"AEA1":
-        error(f"Invalid magic: {magic.hex()}")
+    assert magic == b"AEA1"
 
     profile = int.from_bytes(header[4:7], "little")
-    if profile != AEA_PROFILE__HKDF_SHA256_AESCTR_HMAC__SYMMETRIC__NONE:
-        error(f"Invalid AEA profile: {profile}")
+    assert profile == AEA_PROFILE__HKDF_SHA256_AESCTR_HMAC__SYMMETRIC__NONE
 
     auth_data_blob_size = int.from_bytes(header[8:12], "little")
-
-    if auth_data_blob_size == 0:
-        error("No auth data blob")
+    assert auth_data_blob_size > 0
 
     auth_data_blob = f.read(auth_data_blob_size)
-    if len(auth_data_blob) != auth_data_blob_size:
-        error(f"Expected {auth_data_blob_size} bytes, got {len(auth_data_blob)}")
+    assert len(auth_data_blob) == auth_data_blob_size
 
     assert auth_data_blob[:4]
 
@@ -63,21 +54,18 @@ def get_key(f, verbose: bool = False):
     if verbose:
         pprint(fields, stream=sys.stderr)
 
-    if "com.apple.wkms.fcs-response" not in fields:
-        error("No fcs-response field found!")
-
-    if "com.apple.wkms.fcs-key-url" not in fields:
-        error("No fcs-key-url field found!")
+    assert "com.apple.wkms.fcs-response" in fields
+    assert "com.apple.wkms.fcs-key-url" in fields
 
     fcs_response = json.loads(fields["com.apple.wkms.fcs-response"])
     enc_request = base64.b64decode(fcs_response["enc-request"])
     wrapped_key = base64.b64decode(fcs_response["wrapped-key"])
     url = fields["com.apple.wkms.fcs-key-url"]
 
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
+    with urllib.request.urlopen(url) as r:
+        pem = r.read()
 
-    privkey = KEMKey.from_pem(r.text)
+    privkey = KEMKey.from_pem(pem)
 
     recipient = suite.create_recipient_context(enc_request, privkey)
     pt = recipient.open(wrapped_key)
@@ -89,25 +77,18 @@ def get_key(f, verbose: bool = False):
 
 
 def main(path: str, verbose: bool = False):
-    if path.startswith("http://") or path.startswith("https://"):
-        with requests.get(path, timeout=10, stream=True) as response:
-            response.raise_for_status()
-
-            get_key(response.raw, verbose)
-
-    else:
-        aea_path = Path(path)
-        if not aea_path.exists():
-            error(f"File {path} does not exist")
-
-        with aea_path.open("rb") as f:
-            get_key(f, verbose)
+    aea_path = Path(path)
+    assert aea_path.exists()
+    with aea_path.open("rb") as f:
+        get_key(f, verbose)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get the key for an AEA file or URL")
     parser.add_argument("path", help="Path or URL to the AEA file")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Show verbose output"
+    )
     args = parser.parse_args()
 
     main(args.path, args.verbose)
