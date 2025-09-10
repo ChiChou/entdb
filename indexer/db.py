c4700d62b4ca11111356ac6c098cfb583a0d0259
cc <codecolorist@gmail.com>
2025-09-11 01:35:00 +0200

    add sqlite writer

---
+++ indexer/db.py
@@ -0,0 +1,78 @@
+import plistlib
+import json
+import sqlite3
+
+from pathlib import Path
+
+
+class Writer:
+    def __init__(self, path: str, name: str, build: str, version: str):
+        self.path = path
+        self.conn = sqlite3.connect(self.path)
+
+        self.create_tables()
+        self.osid = self._insert_os(name, build, version)
+
+    def create_tables(self):
+        sql_file = Path(__file__).parent / "schema.sql"
+
+        with open(sql_file, "r") as fp:
+            sql_script = fp.read()
+
+        self.conn.executescript(sql_script)
+        self.conn.commit()
+
+    def _insert_os(self, name: str, version: str, build: str) -> int:
+        cursor = self.conn.execute(
+            "INSERT INTO os (name, version, build) VALUES (?, ?, ?)",
+            (name, version, build),
+        )
+        self.conn.commit()
+        osid = cursor.lastrowid
+        assert osid is not None, "Failed to insert OS entry"
+        return osid
+
+    def insert(self, path: str, xml: bytes):
+        assert len(xml), "XML data cannot be empty"
+
+        d = plistlib.loads(xml)
+        j = json.dumps(d)
+
+        cursor = self.conn.execute(
+            "INSERT INTO bin (osid, path, xml, json) VALUES (?, ?, ?, ?)",
+            (self.osid, path, xml, j),
+        )
+
+        binid = cursor.lastrowid
+        assert binid is not None, "failed to insert row for path {path}"
+
+        for key, val in d.items():
+            self.conn.execute(
+                "INSERT INTO pair (binid, key, value) VALUES (?, ?, ?)",
+                (binid, key, json.dumps(val)),
+            )
+
+        self.conn.commit()
+
+
+class Reader:
+    def __init__(self, path: str):
+        self.path = path
+        self.conn = sqlite3.connect(self.path)
+
+    def metadata(self, build: str):
+        cursor = self.conn.execute('SELECT name, version FROM os WHERE build=?', (build,))
+        name, version = cursor.fetchone()
+        return dict(name=name, build=build, version=version)
+
+    def paths(self, build: str) -> list[str]:
+        cursor = self.conn.execute("""
+            SELECT path FROM bin JOIN os ON bin.osid=os.id
+            WHERE os.build=?""", (build,))
+        return [row[0] for row in cursor.fetchall()]
+
+    def keys(self, build: str):
+        cursor = self.conn.execute("""
+            SELECT distinct key FROM pair JOIN bin ON pair.binid=bin.id
+            JOIN os ON bin.osid=os.id WHERE os.build=?""", (build,))
+        return [row[0] for row in cursor.fetchall()]
---
+++ indexer/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS "os";
+DROP TABLE IF EXISTS "bin";
+DROP TABLE IF EXISTS "bin_osid";
+DROP TABLE IF EXISTS "pair";
+DROP TABLE IF EXISTS "pair_binid";
+
+CREATE TABLE "os" (
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "name" VARCHAR(32) NOT NULL,
+    "build" VARCHAR(32) NOT NULL,
+    "version" VARCHAR(32) NOT NULL);
+
+CREATE TABLE "bin" (
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "osid" INTEGER NOT NULL,
+    "path" TEXT NOT NULL,
+    "xml" TEXT NOT NULL,
+    "json" TEXT NOT NULL,
+    FOREIGN KEY ("osid") REFERENCES "os" ("id"));
+
+CREATE INDEX "bin_osid" ON "bin" ("osid");
+
+CREATE TABLE "pair" (
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "binid" INTEGER NOT NULL,
+    "key" VARCHAR(255) NOT NULL,
+    "value" TEXT NOT NULL,
+    FOREIGN KEY ("binid") REFERENCES "bin" ("id"));
+
+CREATE INDEX "pair_binid" ON "pair" ("binid");
