aff7aaffe03a6107c19bd804c3c26f7267ca843b
cc <codecolorist@gmail.com>
2025-09-18 18:48:32 +0200

    finish theapplewiki api

--- data/theapplewiki.com/api.py
+++ data/theapplewiki.com/api.py
@@ -11,13 +11,11 @@ def concat_url(params: str):
     return base_url + urllib.parse.quote(page)
 
 
-def format_url_get_page(model: str, version: str):
+def format_url_get_name(model: str, build: str):
     url_template = f"""
         /-5B-5B:Keys:-2B-5D-5D
         /-5B-5BHas firmware device::{model}-5D-5D
-        /-5B-5BHas firmware version::{version}-5D-5D
-        /-3FHas download URL=url
-        /-3FHas firmware baseband=baseband
+        /-5B-5BHas firmware build::{build}-5D-5D
         /-3FHas firmware build=build
         /-3FHas firmware codename=codename
         /-3FHas firmware device=device
@@ -38,8 +36,6 @@ def format_url_fetch_key(page: str):
         /-3FHas firmware device=device
         /-3FHas key=key
         /-3FHas key IV=iv
-        /-3FHas key KBAG=kbag
-        /-3FHas key DevKBAG=devkbag
         /mainlabel=filename
         /format=json
         /type=simple
@@ -48,20 +44,32 @@ def format_url_fetch_key(page: str):
     return concat_url(url_template)
 
 
+def fetch(url: str) -> dict[str, dict]:
+    req = urllib.request.Request(
+        url, headers={"User-Agent": "HTTPie/3.2.4", "Accept": "application/json"}
+    )
+
+    resp = urllib.request.urlopen(req).read()
+    result: dict[str, dict] = json.loads(resp)
+    return result
+
+
+def get_page_name(model: str, build: str) -> str:
+    url = format_url_get_name(model, build)
+    result = fetch(url)
+    val = next(iter(result.values()))
+    name, *_ = val["name"]
+    return name
+
+
+def fetch_page(key: str) -> dict[str, dict]:
+    url = format_url_fetch_key(key)
+    return fetch(url)
+
+
 if __name__ == "__main__":
     import sys
 
-    url1 = format_url_get_page("iPhone5,4", "7.0.5")
-    sys.stderr.write(url1 + "\n")
-    req1 = urllib.request.Request(
-        url1,
-        headers={
-            'User-Agent': 'HTTPie/3.2.4',
-            'Accept': 'application/json'
-        }
-    )
-
-    resp1 = urllib.request.urlopen(req1).read()
-    result1: dict[str, dict] = json.loads(resp1)
-    val = next(iter(result1.values()))
-    print(val)
+    key = get_page_name(sys.argv[1], sys.argv[2])
+    content = fetch_page(key)
+    print(content)
