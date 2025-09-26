9f0b31e2867c921cd3fdbeccfd253dc45880263b
cc <codecolorist@gmail.com>
2025-09-27 01:36:40 +0200

    support groups

--- src/components/oslist.tsx
+++ src/components/oslist.tsx
@@ -8,46 +8,60 @@ import { addBasePath } from "@/lib/env";
 import { Skeleton } from "./ui/skeleton";
 import { Checkbox } from "./ui/checkbox";
 
+interface Group {
+  name: string;
+  list: OS[];
+}
+
+function responseOK(r: Response) {
+  if (!r.ok) {
+    throw new Error(`Failed to fetch resource at ${r.url}`);
+  }
+  return r;
+}
+
 export default function OSList() {
-  const [list, setList] = useState<OS[]>([]);
-  const [includeMinorVersions, setIncludeMinorVersions] = useState(false);
-  const [filtered, setFiltered] = useState<OS[]>([]);
+  // const [includeMinorVersions, setIncludeMinorVersions] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [groups, setGroups] = useState<Group[]>([]);
 
   useEffect(() => {
     setLoading(true);
-    fetch(addBasePath("/data/list"))
-      .then((r) => {
-        if (!r.ok) {
-          throw new Error("Failed to fetch OS list");
-        }
-        return r;
+
+    fetch(addBasePath("/data/groups.json"))
+      .then(responseOK)
+      .then((r) => r.json() as Promise<string[]>)
+      .then(async (groupList: string[]) =>
+        Promise.all(
+          groupList.map(async (group) => {
+            const response = await fetch(
+              addBasePath(`/data/${group}/list.json`),
+            ).then(responseOK);
+
+            const data = await response.json();
+
+            return {
+              name: group,
+              list: data,
+            };
+          }),
+        ),
+      )
+      .then((groups) => {
+        setGroups(groups);
       })
-      .then((r) => r.text())
-      .then((txt) => {
-        const lines = txt.split("\n").filter((line) => line.trim() !== "");
-        return lines.map((id) => {
-          const [version, build] = id.split("_");
-          return { id, version, build };
-        });
-      })
-      .then(setList)
       .finally(() => setLoading(false));
   }, []);
-
-  useEffect(() => {
-    setFiltered(list.filter((os) => os.version.endsWith(".0")));
-  }, [list]);
 
   return (
     <div>
       {loading && <Skeleton className="h-[20px] w-[100px] rounded-full" />}
 
-      {!loading && list.length === 0 && (
+      {!loading && groups.length === 0 && (
         <div className="text-center">Failed to fetch OS list</div>
       )}
 
-      <header className="mb-4">
+      {/*<header className="mb-4">
         <Checkbox
           id="select-all"
           className="mr-2"
@@ -59,23 +73,28 @@ export default function OSList() {
         <label htmlFor="select-all" className="text-lg font-medium">
           Include Minor Versions
         </label>
-      </header>
+      </header>*/}
 
-      <ul className="grid grid-cols-2 xl:grid-cols-4 gap-4">
-        {(includeMinorVersions ? list : filtered).map((os, index) => (
-          <li key={index} className="list-none">
-            <Link
-              href={`/os/keys?os=${os.id}`}
-              className="block p-4 border rounded-lg shadow-sm hover:shadow-md transition-all hover:bg-gray-50"
-            >
-              <div className="flex justify-between items-center">
-                <h2 className="text-lg">{os.version}</h2>
-                <div className="text-sm text-gray-500">{os.build}</div>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {groups.map((group) => (
+        <section key={group.name} className="my-6">
+          <h2 className="text-2xl font-light my-4">{group.name}</h2>
+          <ul className="grid grid-cols-2 xl:grid-cols-4 gap-4">
+            {group.list.map((os, index) => (
+              <li key={index} className="list-none">
+                <Link
+                  href={`/os/keys?os=${group.name}/${os.version}_${os.build}`}
+                  className="block p-4 border rounded-lg shadow-sm hover:shadow-md transition-all hover:bg-gray-50"
+                >
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-lg">{os.name}</h2>
+                    <div className="text-sm text-gray-500">{os.build}</div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
     </div>
   );
 }
