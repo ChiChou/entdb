352081dcc4b5840762e1d87485d9d457421e86e3
cc <codecolorist@gmail.com>
2025-09-27 16:14:33 +0200

    fix layout on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import OSList from "@/components/oslist";
 export default async function Home() {
   return (
     <div className="font-sans">
-      <h1 className="text-4xl text-center mt-16">Entitlement Database</h1>
-      <div className="items-center justify-center sm:m-4 md:m-16">
+      <h1 className="text-2xl md:text-4xl text-center md-mt-16 mt-8">
+        Entitlement Database
+      </h1>
+      <div className="items-center justify-center m-4 md:m-16">
         <OSList />
       </div>
     </div>
